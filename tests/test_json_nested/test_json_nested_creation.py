7524fcc7560707ef39d46b4895bf6065c1991aa2
luchanos <Forgotten1000>
2021-09-20 02:07:32 +0300

    [upd] tests

--- tests/test_json_nested/test_json_nested_creation.py
+++ tests/test_json_nested/test_json_nested_creation.py
@@ -5,12 +5,12 @@ from tests.test_json_nested.data_for_testing import sample_input, sample_output,
 
 
 @pytest.mark.parametrize(
-    "sample_input, keys_priority, sample_output",
+    "test_sample_input, keys_priority, test_sample_output",
     [
         (sample_input, ["currency", "country", "city"], sample_output),
         (sample_input, ["city"], sample_output_2),
         (sample_input, ["amount", "currency"], sample_output_3),
     ],
 )
-def test_make_nested_json(sample_input, keys_priority, sample_output):
-    assert make_nested_json(sample_input, *keys_priority) == sample_output
+def test_make_nested_json(test_sample_input, keys_priority, test_sample_output):
+    assert make_nested_json(test_sample_input, *keys_priority) == test_sample_output
